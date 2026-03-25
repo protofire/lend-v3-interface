8168dde2d3f679c28e34b89082de6694c5838003
Emosaddd <pl.maksim.krukovich@gmail.com>
2026-03-25 14:14:57 +0100

    feat: disable permit, add PST logo

--- src/components/transactions/FlowCommons/RightHelperText.tsx
+++ src/components/transactions/FlowCommons/RightHelperText.tsx
@@ -1,11 +1,9 @@
 import { ExternalLinkIcon } from '@heroicons/react/outline';
 import { Trans } from '@lingui/macro';
-import { Box, Link, SvgIcon, Typography } from '@mui/material';
-import { ApprovalMethodToggleButton } from 'src/components/transactions/FlowCommons/ApprovalMethodToggleButton';
+import { Box, Link, SvgIcon } from '@mui/material';
 import { MOCK_SIGNED_HASH } from 'src/helpers/useTransactionHandler';
 import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
 import { useRootStore } from 'src/store/root';
-import { ApprovalMethod } from 'src/store/walletSlice';
 
 export type RightHelperTextProps = {
   approvalHash?: string;
@@ -19,23 +17,13 @@ const ExtLinkIcon = () => (
 );
 
 export const RightHelperText = ({ approvalHash, tryPermit }: RightHelperTextProps) => {
-  const { walletApprovalMethodPreference, setWalletApprovalMethodPreference } = useRootStore();
+  const { walletApprovalMethodPreference } = useRootStore();
   const usingPermit = tryPermit && walletApprovalMethodPreference;
   const { currentNetworkConfig } = useProtocolDataContext();
   const isSigned = approvalHash === MOCK_SIGNED_HASH;
   // a signature is not submitted on-chain so there is no link to review
-  if (!approvalHash && !isSigned && tryPermit)
-    return (
-      <Box sx={{ display: 'inline-flex', alignItems: 'center', mb: 2 }}>
-        <Typography variant="subheader2" color="text.secondary">
-          <Trans>Approve with</Trans>&nbsp;
-        </Typography>
-        <ApprovalMethodToggleButton
-          currentMethod={walletApprovalMethodPreference}
-          setMethod={(method: ApprovalMethod) => setWalletApprovalMethodPreference(method)}
-        />
-      </Box>
-    );
+  if (!approvalHash && !isSigned && tryPermit) return <></>;
+
   if (approvalHash && !usingPermit)
     return (
       <Box
